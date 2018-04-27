9d1bdf0a94cb447a7b0e65bf44fe638a852c2d5c
wanessaramos <wanessinha_ramos@yahoo.com.br>
2018-04-27 11:35:36 -0300

    Criação da Classe EscolaResouce

--- src/main/java/br/rl/projetoescolarweb/dao/EscolaDao.java
+++ src/main/java/br/rl/projetoescolarweb/dao/EscolaDao.java
@@ -1,9 +1,13 @@
 package br.rl.projetoescolarweb.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.rl.projetoescolarweb.modelo.Escola;
 
 public interface EscolaDao extends JpaRepository<Escola,Long>{
+
+	public List<Escola> findByNome(String nome);
 
 }
