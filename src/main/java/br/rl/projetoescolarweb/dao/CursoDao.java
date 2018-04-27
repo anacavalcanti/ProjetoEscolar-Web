225abb482c47767377d67bdb0625b6634bf6f9eb
wanessaramos <wanessinha_ramos@yahoo.com.br>
2018-04-27 11:35:09 -0300

    Criação da Classe CursoResouce

--- src/main/java/br/rl/projetoescolarweb/dao/CursoDao.java
+++ src/main/java/br/rl/projetoescolarweb/dao/CursoDao.java
@@ -1,9 +1,12 @@
 package br.rl.projetoescolarweb.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.rl.projetoescolarweb.modelo.Curso;
+import br.rl.projetoescolarweb.modelo.Escola;
 
 public interface CursoDao extends JpaRepository<Curso,Long>{
-
+	public List<Curso> findByNome(String nome);
 }
