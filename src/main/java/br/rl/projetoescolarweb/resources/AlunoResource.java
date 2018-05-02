f16fcb46c080d9c6a37ad185514bd4ee3e2d7da4
wanessaramos <wanessinha_ramos@yahoo.com.br>
2018-05-02 11:44:55 -0300

    Atualização da Classe AlunoResource

--- src/main/java/br/rl/projetoescolarweb/resources/AlunoResource.java
+++ src/main/java/br/rl/projetoescolarweb/resources/AlunoResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import br.rl.projetoescolarweb.dao.AlunoDao;
 import br.rl.projetoescolarweb.modelo.Aluno;
+import br.rl.projetoescolarweb.modelo.Endereco;
 
 @RestController
 @RequestMapping("/aluno")
@@ -18,33 +19,32 @@ public class AlunoResource {
 	AlunoDao alunoRepository;
 	
 	@RequestMapping(value = "carregar", method=RequestMethod.GET)
-	
 	public String carregar() {
 		
 		Aluno aluno = new Aluno();
 		
-		aluno.setNome("Luíz");
-				
+		aluno.setNome("Maria José");
+		aluno.setMatricula("1345");
+		aluno.setEndereco(new Endereco("Rua Marechal", 'A', 320, "Tabuleiro", "Maceio", "AL", "57065-000"));
+			
 		alunoRepository.save(aluno);
 		
 		return "ok";
 	}
 	
-	@RequestMapping(value = "{id}/detalhes", method = RequestMethod.GET)
-	
+	@RequestMapping(value = "/{id}/detalhes", 
+			method = RequestMethod.GET)
 	public Aluno buscar(@PathVariable("id") Long id) {
-		
 		return alunoRepository.getOne(id);
 	}
 	
-	@RequestMapping(value = "listar/todos", method=RequestMethod.GET)
+	@RequestMapping(value = "/listar", method=RequestMethod.GET)
 	
 	public List<Aluno> listar(){
-		
 		return alunoRepository.findAll();
 	}
 	
-	@RequestMapping(value = "pesquisar", method = RequestMethod.GET)
+	@RequestMapping(value = "/pesquisar", method = RequestMethod.GET)
 	
 	public List<Aluno> pesquisar(
 			
