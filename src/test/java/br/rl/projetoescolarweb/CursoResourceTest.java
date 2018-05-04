24c1683dae78f5e1eb195be03c6c72e5c505c3f6
wanessaramos <wanessinha_ramos@yahoo.com.br>
2018-05-04 10:20:55 -0300

    Criação da Classe CursoResourceTest

---
+++ src/test/java/br/rl/projetoescolarweb/CursoResourceTest.java
@@ -0,0 +1,57 @@
+package br.rl.projetoescolarweb;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.rl.projetoescolarweb.dao.CursoDao;
+import br.rl.projetoescolarweb.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class CursoResourceTest {
+	
+	final String BASE_PATH = "http://localhost:8080/curso";
+	
+	@Autowired
+	private CursoDao cursoRepository;
+	
+	private RestTemplate restTemplate;
+	
+	private ObjectMapper MAPPER = new ObjectMapper();
+
+	@Before
+	public void setUp() {
+		restTemplate = new RestTemplate();
+		cursoRepository.deleteAll();
+		
+		
+		
+	}
+
+	@Test
+	public void deveFuncionarAListagemDeTodosOsAlunos() throws JsonParseException, JsonMappingException, IOException {
+		String response = restTemplate.getForObject(BASE_PATH + "/listar/todos",String.class);
+		
+		List<Curso>  cursos = MAPPER.readValue(response,
+				MAPPER.getTypeFactory().constructCollectionLikeType(List.class, Curso.class));
+		
+		int tamanhoEsperadoDaLista = 3;
+		
+		assertEquals(tamanhoEsperadoDaLista,cursos.size());
+	}
+
+}
